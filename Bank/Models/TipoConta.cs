3b6e1c0a9df2c369571dd91ecdf50eef3553a9f2
lucasAS1 <lucas.a.silva1@outlook.com>
2021-04-19 01:23:48 -0300

    adição de sumário ao enum TipoConta

--- Bank/Models/TipoConta.cs
+++ Bank/Models/TipoConta.cs
@@ -1,6 +1,10 @@
 using System;
 
 namespace Bank.Models{
+
+    /// <summary>
+    /// Enum para identificar o tipo de conta, 1 - Pessoa Física, 2 - Pessoa Juridica
+    /// </summary>
     public enum TipoConta{
         PessoaFisica=1,
         PessoaJuridica=2
